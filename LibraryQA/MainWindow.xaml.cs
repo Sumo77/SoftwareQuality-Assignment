5c7523f5595dd22efd21ccd6b3af4bad01274819
dashka! <zkc9669@autuni.ac.nz>
2026-08-19 16:49:39 +1200

    member view with login fixes (needs clean up)

--- LibraryQA/MainWindow.xaml.cs
+++ LibraryQA/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using LibraryQA.Core.Models;
+using LibraryQA.Core.Services;
 using LibraryQA.Views;
 
 namespace LibraryQA
@@ -9,40 +10,49 @@ namespace LibraryQA
         public MainWindow()
         {
             InitializeComponent();
-            //change later 
-            RootGrid.Children.Add(new MemberView(1));
+            ShowLogin();
         }
 
-        // TODO: unused while testing MemberView with a hardcoded id above.
-        // Uncomment / fix once real login wiring (with a real memberId) is ready.
-        //
-        // private void ShowLogin()
-        // {
-        //     var loginView = new LoginView();
-        //     loginView.LoginSucceeded += LoginView_LoginSucceeded;
-        //
-        //     RootGrid.Children.Clear();
-        //     RootGrid.Children.Add(loginView);
-        // }
-        //
-        // private void LoginView_LoginSucceeded(object? sender, UserRole role)
-        // {
-        //     if (role == UserRole.Staff)
-        //     {
-        //         var staffView = new StaffView();
-        //         staffView.LogoutRequested += (_, _) => ShowLogin();
-        //
-        //         RootGrid.Children.Clear();
-        //         RootGrid.Children.Add(staffView);
-        //     }
-        //     else
-        //     {
-        //         var memberView = new MemberView(1 /* TODO: real memberId */);
-        //         memberView.LogoutRequested += (_, _) => ShowLogin(); 
-        //
-        //         RootGrid.Children.Clear();
-        //         RootGrid.Children.Add(memberView);
-        //     }
-        // }
+        private void ShowLogin()
+        {
+            var loginView = new LoginView();
+            loginView.LoginSucceeded += LoginView_LoginSucceeded;
+
+            RootGrid.Children.Clear();
+            RootGrid.Children.Add(loginView);
+        }
+
+        private void LoginView_LoginSucceeded(object? sender, UserRole role)
+        {
+            var loginView = sender as LoginView;
+            string username = loginView?.UsernameBox.Text.Trim() ?? "";
+
+            var authService = new AuthenticationService(App.ConnectionString);
+            int? accountId = authService.GetAccountId(username);
+
+            if (accountId == null)
+            {
+                MessageBox.Show("Could not resolve the logged-in account. Please try again.",
+                    "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (role == UserRole.Staff)
+            {
+                var staffView = new StaffView();
+                staffView.LogoutRequested += (_, _) => ShowLogin();
+
+                RootGrid.Children.Clear();
+                RootGrid.Children.Add(staffView);
+            }
+            else
+            {
+                var memberView = new MemberView(accountId.Value);
+                memberView.LogoutRequested += (_, _) => ShowLogin();
+
+                RootGrid.Children.Clear();
+                RootGrid.Children.Add(memberView);
+            }
+        }
     }
 }
